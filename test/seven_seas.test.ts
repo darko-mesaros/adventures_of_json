f01d0078f750f9ebff96025df54a9c0ff2ad9938
Darko Mesaros <darko.subotica@gmail.com>
2024-08-08 09:47:26 -0700

    Initial commit

---
+++ test/seven_seas.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SevenSeas from '../lib/seven_seas-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/seven_seas-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SevenSeas.SevenSeasStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
